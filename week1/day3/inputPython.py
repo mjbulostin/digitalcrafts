20a0cd8dbcc17dddda128aee7830edfd25b3647f
mjbulostin <mjbulostin@gmail.com>
2021-03-10 12:55:02 -0500

    added first and last name program

---
+++ week1/day3/inputPython.py
@@ -0,0 +1,4 @@
+first_name_of_user = input("What is your first name?")
+last_name_of_user = input("What is your last name?")
+print("Hello %s %s, welcome to python!" %
+      (first_name_of_user, last_name_of_user))
