0be97893012dcd5dada3fa91f36dda90c95fa0d0
mjbulostin <mjbulostin@gmail.com>
2021-03-10 12:53:28 -0500

    first and last name program

--- week1/day3/pythonExample.py
+++ week1/day3/pythonExample.py
@@ -4,12 +4,16 @@
 greeting = "Hello Digital Crafts Family"
 # Integer === whole number, 1, 23, 934234, 9392
 classSize = 23
+dirPlusTeacher = 4
 # Float === 4.5, 4.5555555565, 4.5656464646546545646435634563
 floatExample = 2.323
 # Boolean === true or false, true, false, returns a truthy or falsy value
-sumOfNumbers = 1 + 1
+sumOfClass = dirPlusTeacher + classSize
 # Name, assignment operator (=), value that I am assigning
 variableName = True
+somethingToCallIt = ["students", 23, True]
+somethingToCallIt = {"hellowPythonWorld": 23, "We are students": 24.5}
+print("The size of our class is", sumOfClass)
 # List, sequence of values followed by [value1, value2, etc]
 # [1,2,3]
 # ["strings", "string2", "blahblahblah"]
@@ -21,7 +25,7 @@ variableName = True
 # # Sequence of labeled values {key: value}
 # {"stringName": "StringValue"}
 # {"FavoriteColor": "Blue"}
-print("Hello World!")
-print("My name is Michael, and this is my first program!")
-print(classSize)
-print("Hi")
+# print("Hello World!")
+# print("My name is Michael, and this is my first program!")
+# print(classSize)
+# print("Hi")
