c06e93b48424224d557ba1f2497805f89023d895
mjbulostin <mjbulostin@gmail.com>
2021-05-14 14:41:34 -0400

    object to array in JavaScript

---
+++ week10/Day50/objectToArray.js
@@ -0,0 +1,23 @@
+const obj = {
+    person: {
+        name: 'Tyler',
+        age: 28,
+        height: '180cm'
+    },
+    hobbies: {
+        sport: 'Football',
+        game: 'Overwatch'
+    }
+}
+
+const objKeys = Object.keys(obj);
+
+console.log(objKeys);
+
+const objValues = Object.values(obj);
+
+console.log(objValues);
+
+const objEntries = Object.entries(obj);
+
+console.log(objEntries);
