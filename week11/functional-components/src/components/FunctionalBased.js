c6b767ba3d50d81b226b0d06960af96b3b7fd650
mjbulostin <mjbulostin@gmail.com>
2021-05-20 11:13:58 -0400

    before we wave goodbye to the functional based component

---
+++ week11/functional-components/src/components/FunctionalBased.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const FunctionalBased = () => {
+    const [loadingMessage, setLoadingMessage] = useState("loading");
+    const [status, setStatus] = useState("Code Red");
+    return (
+        <div>
+            <h1>Functional Component</h1>
+            {loadingMessage}
+            {status}
+            <button onClick={() => setLoadingMessage("finished")}>Finished Loading</button>
+            <button onClick={() => setStatus("Code Green")}>Status</button>
+        </div>
+    );
+};
+
+export default FunctionalBased;
