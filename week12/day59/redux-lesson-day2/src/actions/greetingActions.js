35a812b7b21a76b6ace57f30b367f26dad89bc26
mjbulostin <mjbulostin@gmail.com>
2021-05-27 15:47:18 -0400

    fixed redux-logger

--- week12/day59/redux-lesson-day2/src/actions/greetingActions.js
+++ week12/day59/redux-lesson-day2/src/actions/greetingActions.js
@@ -1,3 +1,11 @@
-export const changeDataToMike = () => {
-    return { type: "INSERT_OBJECT" };
-};
+export const changeFakeDataToJoe = (dispatch) => {
+    return dispatch({ type: "INSERT_FAKE_JOE" });
+  };
+  
+  export const changeGovOfficials = (dispatch) => {
+    return dispatch({ type: "CHANGE_GOV_OFFICIAL" });
+  };
+  
+  export const removeElon = (dispatch) => {
+    return dispatch({ type: "REMOVE_DOGE_LORD" });
+  };
--- week12/day59/redux-lesson-day2/src/index.js
+++ week12/day59/redux-lesson-day2/src/index.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import App from './App';
 import logger from "redux-logger";
-import {reducer } from "./reducers/reducer"
+import { reducer } from "./reducers/reducer";
+import App from "./App";
 
-const store = createStore(applyMiddleware(logger));
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
